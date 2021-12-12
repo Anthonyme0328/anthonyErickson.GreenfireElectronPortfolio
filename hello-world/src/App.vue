d4cea8bd964ca9d597ccb4fbb7c612b9a9d856f5
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2021-12-12 15:07:25 -0500

    working on api call

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,30 @@
 <template>
- 
-
- 
-  <HelloWorld msg= "hello world"/>
+  <QrCodes />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QrCodes from './components/QrCodes.vue'
 
+// axios.get("https://api.coingecko.com/api/v3/simple/price?ids=tezos%2Calgorand%2Csolana&vs_currencies=usd")
+// .then(res => {
+//         this.res = res,
+//         this.data[0] = "Tezos current price:" + res.data.tezos.usd 
+//         this.data[1] = "Algorand current price:" + res.data.algorand.usd
+//         this.data[2] = "Solana current price:" + res.data.solana.usd 
+//         })
+
+// const getPrices = async() => {
+//   await fetch("https://api.coingecko.com/api/v3/simple/price?ids=tezos%2Calgorand%2Csolana&vs_currencies=usd")
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+// }
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QrCodes
   }
 }
 </script>
---
+++ hello-world/src/components/QrCodes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Qr_div">
+    <img class= "QR_codes" src="../assets/AME_QR.png" alt="AME code">    
+    <img class= "QR_codes" src="../assets/Tezos_QR.png" alt="tezos code">
+    <img class= "QR_codes" src="../assets/Solana_QR.png" alt="solana code">
+    <img class= "QR_codes" src="../assets/Algorand_QR.png" alt="algorand code">
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'QrCodes',
+}
+</script>
+
+
+<style scoped> 
+.Qr_div{
+  display: flex;
+  justify-content: space-between;
+ }
+
+ .QR_codes{
+   margin-top: 10%;
+   height:100px;
+   width:100px;
+ }
+
+
+</style>
